4a58eceb29cb0bee5cfcd5e0d3f5d130d840ae0d
Poulami807 <poulamiganguly10@gmail.com>
2022-07-09 18:05:38 +0530

    functionality added

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-var field = document.getElementById("field")
+var field = document.getElementById("field");
+var btn = document.querySelectorAll(".ele");
+btn.forEach((ele) => {
+  var val = ele.innerText;
+  ele.addEventListener("click", () => {
+    if (ele.id == "cls") field.innerText = "";
+    else if (ele.id == "eq") result();
+    else {
+      display(val);
+    }
+  });
+});
+
+function display(v) {
+  field.innerText += v;
+}
+function result() {
+  var res = math.evaluate(field.innerText);
+  console.log(res);
+  display(res);
+}
